2b0373516591b94185fae6824b3e47cdd511720c
avan411 <avan411@aucklanduni.ac.nz>
2021-04-25 18:20:31 +1200

    directdirect access to databaseRevert "direct access to database"
    This reverts commit 128f70f82f58a355d2310f4b59ee87385d3cf2db.

---
+++ Database_access.py
@@ -0,0 +1,36 @@
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+pw = "Ashley@2802" # IMPORTANT! Put your MySQL Terminal password here.
+db = "Seeker
+" # This is the name of the database we will create in the next step - call it whatever you like.
+
+connection = create_server_connection("localhost", "root", pw)
+
+# Assign our SQL command to a python variable using triple quotes to create a multi-line string
+create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
+
+connection = create_db_connection("localhost", "root", pw, db) # Connect to the Database
+execute_query(connection, create_teacher_table) # Execute our defined query
